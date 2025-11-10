c04ef239707f9edc0967b5809687e7bca359346b
unknown <mdani.csit@gmail.com>
2025-11-10 13:53:44 +0000

    second commit

---
+++ src/pages/index.js
@@ -0,0 +1,4 @@
+export { default as Home } from './Home/Home';
+export { default as Men } from './Men/Men';
+export { default as Women } from './Women/Women';
+export { default as Kids } from './Kids/Kids';
---
+++ src/utils/constants.js
@@ -0,0 +1,12 @@
+export const CATEGORIES = {
+    MEN: 'men',
+    WOMEN: 'women',
+    KIDS: 'kids'
+  };
+  
+  export const SORT_OPTIONS = [
+    { value: 'featured', label: 'Sort by: Featured' },
+    { value: 'price-low', label: 'Price: Low to High' },
+    { value: 'price-high', label: 'Price: High to Low' },
+    { value: 'newest', label: 'Newest First' }
+  ];
